4beaea445af4fd89b8eec29ce288f4cd21f1061e
Timcho018 <76828089@continental.edu.pe>
2025-10-07 20:12:26 +0200

    build: generar dist para vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
+// @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static";
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "static",
+  adapter: vercel(),
 });
